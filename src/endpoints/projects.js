a68234236bcf88968fef7563ccd4f3ae3b2c3f89
Adam Kohari <Adam_Kohari@epam.com>
2021-05-04 15:10:47 +0200

    createProject endpoint

--- src/endpoints/projects.js
+++ src/endpoints/projects.js
@@ -29,6 +29,50 @@ const getProjects = (req, res) => {
 
 
 const createProject = (req, res) => {
+    try {
+        const userId = auth(req);
+        if (!userId) {
+            res.json({status: 'FAIL', error: 'UNAUTHORIZED'});
+            return;
+        }
+        const userProjectsTable = global.kanban.collection('userProjects');
+        const usersTable = global.kanban.collection('users');
+
+        const usersArray = [{userId: userId}];
+        const memberEmails = req.body.emails;
+
+        usersTable.find({email: {$in: memberEmails}}, (err, data) => {
+            if (err) {
+                res.json({ status: 'FAIL', error: err });
+            } else {
+                data.toArray((err, result) => {
+                    if (err) {
+                        res.json({ status: 'FAIL', error: err });
+                    } else {
+                        result.forEach(user => {
+                            usersArray.push({
+                                userId: user._id.toString()
+                            });
+                        });
+
+                        if (usersArray.length !== memberEmails.length + 1) {
+                            res.json({status: 'FAIL', error: 'INVALID_EMAILS'});
+                        } else {
+                            userProjectsTable.insertOne({
+                                users: usersArray,
+                                name: req.body.name,
+                                shortName: req.body.shortName
+                            });
+                    
+                            res.json({status: 'OK', data: 'PROJECT_CREATION_SUCCESS'});
+                        }
+                    }
+                });
+            }
+        });
+    } catch (ex) {
+        res.json({status: 'FAIL', error: ex.toString()});
+    }
 };
 
 exports.getProjects = getProjects;
